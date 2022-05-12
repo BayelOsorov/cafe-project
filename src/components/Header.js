65e4dc92d07996840ad67d329e03c4a8ae4f00b8
Adelina2702 <aadelya2702@gmail.com>
2022-05-12 20:13:51 +0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import classes from "./HeaderModule.scss";
+import { BiMenuAltRight } from "react-icons/bi";
+// import { AiOutlineClose } from "react-icons/ai";
+
+export default function Header() {
+  return (
+        <header className={classes.header}>
+            <div className={classes.header_content}>
+                <h2>NavBar</h2>
+            </div>
+            <div className={classes.header__content__toggle}>
+                        <BiMenuAltRight/>
+                </div>
+            <nav className={classes.header_content_nav}>
+               <p>Доставка готовых блюд в Екатеринбурге</p>
+                <ul>
+                    <li>+7 (953) 601-96-14</li>
+                    <li>
+                        <p>ГОСТИОН <span>food</span></p> 
+                    </li>
+                    <li>
+                        <a href='' >Личный кабинет</a>
+                    </li>
+                    <li>
+                        <a href='' >Корзина</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+  )
+}
