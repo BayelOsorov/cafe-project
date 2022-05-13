5b90623ebfb887c6559cc34925bf71e0795d1fe2
AbdullaevN <nurlanabdullaev9820@gmail.com>
2022-05-13 13:22:47 +0600

    profilePage

--- src/components/Auth/LoginButton.js
+++ src/components/Auth/LoginButton.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+      <Link to="profile">
+        <button>Profile</button>
+      </Link>
+    </>
+  );
 };
 
 export default LoginButton;
