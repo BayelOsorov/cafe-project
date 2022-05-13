894894962eacb94423cd65b692b25bb870ac7954
AbdullaevN <nurlanabdullaev9820@gmail.com>
2022-05-13 17:03:40 +0600

    store-begin

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import { Auth0Provider } from "@auth0/auth0-react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
@@ -10,11 +12,13 @@ const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>
+  <Provider store={store}>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  </Provider>
 );
